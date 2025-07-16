a7f9fa8f43a637c84805702d719342c255e7417f
Tyler <tyler@vxlabs.co>
2025-07-16 16:04:01 -0400

    Fix routing issue - add wildcard to dashboard route to enable nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard/*" element={<Dashboard />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
